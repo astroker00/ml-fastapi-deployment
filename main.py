7680c041b480f57f111b5861361eb98148dd1f96
astroker00 <astroker920@gmail.com>
2026-01-02 20:40:52 +0300

    edit front-end

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 import joblib
+import numpy as np
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="."), name="static")
 
 logistic_model = joblib.load("logistic_model.joblib")
 tree_model = joblib.load("decision_tree_model.joblib")
 
 class InputData(BaseModel):
     features: list
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    with open("index.html", "r") as f:
+        return f.read()
 
 @app.post("/predict/logistic")
 def predict_logistic(data: InputData):
